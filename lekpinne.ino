4c25af7e668b8363cdbb08d060b4ac800c7fd6d3
Strideorlov <36800885+Strideorlov@users.noreply.github.com>
2018-02-24 21:30:03 +0100

    Add files via upload

---
+++ lekpinne.ino
@@ -0,0 +1,37 @@
+
+//
+//--------------------------------------------------------------------
+
+
+#include <Joystick.h>
+
+// Variable
+
+int gas = A0;
+
+int gasValue = 0;
+int gasValuebyte = 0;
+
+
+
+// init joystick libary
+void setup() {
+  Joystick.begin();
+}
+
+void loop() {
+  
+  // Gas
+  gasValue = analogRead(gas);
+  if (gasValue >= 1) {
+  gasValuebyte = gasValue / 4 ;
+   }
+   else
+   {
+    gasValuebyte = 0 ;
+   }
+  Joystick.setThrottle(gasValuebyte);
+  delay(1); 
+}
+
+ 
